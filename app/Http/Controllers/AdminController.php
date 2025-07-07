a1951b33e643312571a3cbd114e9538d002453f7
vaghasiyakalpesh <148939090+vaghasiyakalpesh@users.noreply.github.com>
2025-07-07 22:40:48 +0530

    set template and make table, form, dashboard, loginpage

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function form()
+    {
+        return view('admin.form');
+    }
+
+    public function table()
+    {
+        return view('admin.table');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('admin/form', [AdminController::class, 'form'])->name('admin.form');
+Route::get('admin/table', [AdminController::class, 'table'])->name('admin.table');
